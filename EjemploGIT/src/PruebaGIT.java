619c300c932b06d4cf346caf2d10a89b59b40c56
tiomao <tiomao@DESKTOP-C1MPS3K>
2021-09-30 19:10:44 +0200

    Mas informacion printer

--- EjemploGIT/src/PruebaGIT.java
+++ EjemploGIT/src/PruebaGIT.java
@@ -3,7 +3,8 @@ public class PruebaGIT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("prueba GIT");
+		System.out.println("es un poco complicado...");
 	}
 
 }
